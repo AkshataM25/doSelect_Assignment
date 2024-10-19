4942cf3b4f6d7e58ea5f58a66a7bd33cc6619623
AkshataM25 <akshatamense25@outlook.com>
2024-10-19 13:01:13 +0530

    Add files via upload

---
+++ Question2/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Queston2
+{
+    class Source
+    {
+        public int Add(int a, int b, int c)
+        {
+            return a + b + c;
+        }
+
+        public double Add(double a, double b, double c)
+        {
+            return a + b + c;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
